6f511fe1f531e44fce17980effc36e894aa77bf9
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-03-29 17:37:36 -0500

    Created ironpython test

---
+++ IPyTest.py
@@ -0,0 +1,4 @@
+print("Hello world!")
+
+def simpleFunction():
+	print("This was a simple function")
--- PythonGenerator.cs
+++ PythonGenerator.cs
@@ -4,10 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
 namespace Nurielite
 {
     class PythonGenerator
     {
+		public PythonGenerator() { }
 
+		public void testme()
+		{
+			var ipy = Python.CreateRuntime();
+			dynamic test = ipy.UseFile("IPyTest.py");
+			test.simpleFunction();
+		}
     }
 }
