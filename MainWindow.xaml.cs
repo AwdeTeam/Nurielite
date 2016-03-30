c748de961b8b16db09cbda4823935cb106e3ddfe
AwdeDarkar <darkardengeno@gmail.com>
2016-03-30 17:11:00 -0400

    Forgot what I did

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -325,7 +325,9 @@ namespace Nurielite
 
         private void Button_Click_createNoOp(object sender, RoutedEventArgs e)
         {
-            log("commands work");
+            Representation newrep = AlgorithmLoader.generateRepresentation("unnamed", Representation.AlgorithmFamily.Operation,
+                new Datatype[] { Datatype.getType(0) }, new Datatype[] { Datatype.getType(1) });
+            System.Windows.Forms.MessageBox.Show("My message here");
         }
 	}
 }
